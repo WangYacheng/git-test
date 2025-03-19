ceb7d0230b9def199931242836a6fb80bd17d995
luyao <wangyacheng1106@163.com>
2025-03-19 20:55:19 +0800

    1,静夜思 分支

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            /*
+             *    静夜思
+                床前明月光，
+                疑是地上霜。
+                举头望明月，
+                低头思故乡。
+             */
         }
     }
 }
